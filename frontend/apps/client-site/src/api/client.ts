a3b0912d9eed840ce6adffd17899e801e11ade7a
elsieroyalty <elamegavi62@gmail.com>
2024-07-28 22:50:48 +0000

    new updates

--- frontend/apps/client-site/src/api/client.ts
+++ frontend/apps/client-site/src/api/client.ts
@@ -2,6 +2,6 @@ import { create } from 'apisauce'
 
 // define the api
 export const api = create({
-  baseURL: 'https://0ddb-188-166-171-202.ngrok-free.app/api',
+  baseURL: 'https://76f3-188-166-171-202.ngrok-free.app/api',
   headers: { "Content-Type": "application/json" },
 })
--- frontend/apps/client-site/src/api/schema.ts
+++ frontend/apps/client-site/src/api/schema.ts
@@ -1,11 +1,27 @@
 import { z } from "zod";
 
 export const ProductSchema = z.object({
-    id: z.number(),
-    picture_url: z.string(),
-    model: z.string(),
-    prime: z.string(),
-    createAt: z.string().optional()
+    active_status: z.number().nullable(),
+    alternator: z.string().nullable(),
+    amp_per_phase: z.string().nullable(),
+    color: z.string().nullable(),
+    createdAt: z.string().nullable(),
+    delete_status: z.number().nullable(),
+    description: z.string().nullable(),
+    engine: z.string().nullable(),
+    frequency: z.string().nullable(),
+    fuel_type: z.string().nullable(),
+    id: z.number().nullable(),
+    model: z.string().nullable(),
+    other: z.string().nullable(),
+    picture_url: z.string().nullable(),
+    power: z.string().nullable(),
+    price: z.number().nullable(),
+    prime: z.string().nullable(),
+    size: z.string().nullable(),
+    type: z.string().nullable(),
+    voltage: z.string().nullable(),
+    warranty: z.string().nullable()
 })
 
 export const ProductResponseSchema = z.object({
@@ -15,7 +31,12 @@ export const ProductResponseSchema = z.object({
    
 })
 
-
+export const SingleProductResponseSchema = z.object({
+    response_code: z.string(),
+    response_message: z.string(),
+    data: ProductSchema
+   
+})
 
 
 export type ProductType = z.infer<typeof ProductSchema>
